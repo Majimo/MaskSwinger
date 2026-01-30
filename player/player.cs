f1b43862ef11cf16039c57a106ff5e6708cbd571
Pierre Fervel <pierre.fervel@gmail.com>
2026-01-30 22:33:44 +0100

    Add Player basics

--- player/player.cs
+++ player/player.cs
@@ -1,40 +1,38 @@
 using Godot;
 using System;
+using System.Numerics;
+using Vector2 = Godot.Vector2;
 
 public partial class player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	private float _speed = 400;
+	private Vector2 _velocity = Vector2.Zero;
 
-	public override void _PhysicsProcess(double delta)
+	public override void _Process(double delta)
 	{
-		Vector2 velocity = Velocity;
-
-		// Add the gravity.
-		if (!IsOnFloor())
+		var direction = Vector2.Zero;
+		
+		if (Input.IsActionPressed("ui_up"))
 		{
-			velocity += GetGravity() * (float)delta;
+			direction.Y = -1.0f;
+		}
+		if (Input.IsActionPressed("ui_down"))
+		{
+			direction.Y = 1.0f;
+		}
+		if (Input.IsActionPressed("ui_left"))
+		{
+			direction.X = -1.0f;
+		}
+		if (Input.IsActionPressed("ui_right"))
+		{
+			direction.X = 1.0f;
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-
-		Velocity = velocity;
+		_velocity.X = direction.X * _speed;
+		_velocity.Y = direction.Y * _speed;
+		
+		Velocity = _velocity;
 		MoveAndSlide();
 	}
 }
