3d299fa588abfaafc9de6f90fae7c7fdedc16af9
flo_harreau <f.harreau@gmail.com>
2026-01-31 18:23:34 +0100

    temp

--- Mask/Mask.cs
+++ Mask/Mask.cs
@@ -3,5 +3,27 @@ using System;
 
 public partial class Mask : Node3D
 {
+	private Area3D _area => GetNode<Area3D>("Area");
+	
 	[Export] public PlayerBehavior Behavior;
+
+	public override void _Ready()
+	{
+		_area.BodyEntered += AreaOnBodyEntered;
+	}
+
+	private void AreaOnBodyEntered(Node3D body)
+	{
+		if (Behavior is not null && body is CharacterBody3D playerBody)
+		{
+			var player = playerBody.GetParent<Player>();
+
+			if (player.Behavior is not null)
+			{
+				// TODO : drop mask	
+			}
+			
+			this.Behavior = this.Behavior;
+		}
+	}
 }
---
+++ Mask/MaskSpawner.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class MaskSpawner : Node3D
+{
+    private PackedScene _maskScene;
+    private Mask _currentMask;
+
+    public override void _Ready()
+    {
+        _maskScene = GD.Load<PackedScene>("res://Mask/Mask.tscn");
+        this.GetNode<Timer>("Timer").Timeout += TimerOnTimeout;
+    }
+
+    private void TimerOnTimeout()
+    {
+        if (this._currentMask is null && GD.Randf() < 0.5f)
+        {
+            this._currentMask = _maskScene.Instantiate<Mask>();
+            AddChild(this._currentMask);
+        }
+    }
+}
