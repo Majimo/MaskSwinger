333809dd5e48838099323a77a1c07094dc83c718
JaahKii <mary.florian@protonmail.com>
2026-01-31 16:18:57 +0100

    flower and environment

--- Player/Cooldown.cs
+++ Player/Cooldown.cs
@@ -4,22 +4,22 @@ using System;
 [GlobalClass]
 public partial class Cooldown : Node
 {
-    [Export] public double Duration = 1.0d;
+	[Export] public double Duration = 1.0d;
 
-    private double _current;
-    
-    public override void _Process(double delta)
-    {
-        if (_current > 0) _current -= delta;
-    }
+	private double _current;
+	
+	public override void _Process(double delta)
+	{
+		if (_current > 0) _current -= delta;
+	}
 
-    public bool CanExecute()
-    {
-        return _current <= 0;
-    }
+	public bool CanExecute()
+	{
+		return _current <= 0;
+	}
 
-    public void Restart()
-    {
-        _current = Duration;
-    }
+	public void Restart()
+	{
+		_current = Duration;
+	}
 }
