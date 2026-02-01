bcb0c988483747898c8c4ed2b3e21c934fd32e52
flo_harreau <f.harreau@gmail.com>
2026-02-01 16:08:34 +0100

    dfgdfgdf

--- Mask/resize.gd
+++ Mask/resize.gd
@@ -4,17 +4,16 @@ extends Sprite3D
 var tween: Tween
 
 func _on_texture_changed() -> void:
-	scale = Vector3(0.1, 0.1, 0.1)
+	scale = Vector3(0.2, 0.2, 0.2)
 
 func _ready() -> void:
 	
 	var delay: float = float(bpm) / 60.0
-	var half_delay: float = delay / 2.0
 	
 	if tween:
 		tween.kill() # Abort the previous animation.
 	
 	tween = create_tween()
-	tween.tween_property(self, "rotation_degrees", Vector3(0,360,0), half_delay)
-	tween.tween_property(self, "rotation_degrees", Vector3.ZERO, half_delay)
+	tween.tween_property(self, "rotation_degrees", Vector3(0,360,0), delay)
+	tween.tween_property(self, "rotation_degrees", Vector3.ZERO, delay)
 	tween.set_loops()
